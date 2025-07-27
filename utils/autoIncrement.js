91635b47393c26885b1e6ff7ddfa9814df2e2cdc
Gyana prkakash Khandual <gyanaprakashkhnadual@gmail.com>
2025-07-27 15:29:00 +0530

    Repo cleaned

--- utils/autoIncrement.js
+++
@@ -1,9 +0,0 @@
-const Bug = require('../models/Bug');
-
-// Auto-increment serial number per user
-const AutoIncrement = async (userId) => {
-  const lastBug = await Bug.findOne({ user: userId }).sort({ serialNumber: -1 });
-  return lastBug ? lastBug.serialNumber + 1 : 1;
-};
-
-module.exports = AutoIncrement;
